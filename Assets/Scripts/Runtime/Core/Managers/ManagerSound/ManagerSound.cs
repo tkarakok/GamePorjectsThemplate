9caacf1999ea3effadf996c9bfe568c8eba0a13d
tkarakok <karakoktunahan78@gmail.com>
2023-10-17 13:18:56 +0300

    sound system

---
+++ Assets/Scripts/Runtime/Core/Managers/ManagerSound/ManagerSound.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Themplate.Runtime.Core.Managers;
+using Themplate.Runtime.Systems.Singleton;
+using Themplate.Runtime.Systems.Sound;
+using UnityEngine;
+
+[RequireComponent(typeof(SoundSystem))]
+public class ManagerSound : Singleton<ManagerSound>
+{
+    private SaveSystem _saveSystem;
+    private SoundSystem _soundSystem;
+    
+    public SoundSystem SoundSystem
+    {
+        get
+        {
+            if (_saveSystem != null && _saveSystem.SaveState != null)
+            {
+                bool isSoundOn = _saveSystem.SaveState.IsVibrationOn;
+           
+                _soundSystem.SetPreventSound(!isSoundOn);
+            }
+     
+            return _soundSystem;
+        }
+        private set { }
+    }
+
+    private void Awake()
+    {
+        _soundSystem = GetComponent<SoundSystem>();
+    }
+
+    private void Start()
+    {
+        _saveSystem = ManagerSave.Instance.SaveSystem;
+    }
+
+}
